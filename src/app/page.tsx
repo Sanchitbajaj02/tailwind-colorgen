a94f670a38a44704f66406169ce476a085a5f5e8
Sanchit Bajaj <55249639+Sanchitbajaj02@users.noreply.github.com>
2025-06-23 16:12:05 +0300

    fix: color fluidity (#5)
    * fix: color fluidity
    
    * update in greetings workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ColorPicker from "@/components/color-picker";
 
 export default function Home() {
   return (
-    <main className="px-4 py-8 max-w-7xl mx-auto">
+    <main className="px-4 py-8 md:px-8 max-w-7xl mx-auto">
       <h1 className="text-4xl font-extrabold text-center mb-2">Tailwind Colorgen</h1>
       <h2 className="text-2xl font-semibold text-center mb-8">Color Palette Generator</h2>
       <ColorPicker />
